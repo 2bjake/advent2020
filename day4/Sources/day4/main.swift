89f89db878eacba0711d5c81027070a0340e7547
2BJake <jakef@fastmail.com>
2020-12-04 08:43:12 -0600

    day 4 cleanup

--- day4/Sources/day4/main.swift
+++ day4/Sources/day4/main.swift
@@ -6,65 +6,71 @@ extension Sequence {
     }
 }
 
+// part 1
 let requiredFields: Set<String> = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
-func isValidPartOneDoc(_ doc: String) -> Bool {
-    let fields = doc.components(separatedBy: .whitespacesAndNewlines).compactMap {
-        $0.components(separatedBy: ":").first
-    }
-    return requiredFields.isSubset(of: fields)
+func hasRequiredFields(_ fields: [String: String]) -> Bool {
+    return requiredFields.isSubset(of: fields.keys)
 }
 
-func isValidPartTwoDoc(_ doc: String) -> Bool {
-    let fields: [String: String] = doc.components(separatedBy: .whitespacesAndNewlines).reduce(into: [:]) { result, field in
+// part 2
+
+func isYear(_ year: String, within range: ClosedRange<Int>) -> Bool {
+    return range.contains(Int(year) ?? -1)
+}
+
+func isValidHeight(_ height: String) -> Bool {
+    guard let value = Int(height.dropLast(2)) else { return false }
+
+    switch height.suffix(2) {
+        case "in": return (59...76).contains(value)
+        case "cm": return (150...193).contains(value)
+        default: return false
+    }
+}
+
+func isValidHairColor(_ color: String) -> Bool {
+    return color.count == 7 && color.first == "#" && Int(color.dropFirst(), radix: 16) != nil
+}
+
+let validEyeColors: Set<String> = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+func isValidEyeColor(_ color: String) -> Bool {
+    return validEyeColors.contains(color)
+}
+
+func isValidPassportId(_ id: String) -> Bool {
+    return id.count == 9 && Int(id) != nil
+}
+
+func hasValidFields(_ fields: [String: String]) -> Bool {
+    guard hasRequiredFields(fields) else { return false }
+    for field in fields {
+        switch field.key {
+            case "byr": guard isYear(field.value, within: 1920...2002) else { return false }
+            case "iyr": guard isYear(field.value, within: 2010...2020) else { return false }
+            case "eyr": guard isYear(field.value, within: 2020...2030) else { return false }
+            case "hgt": guard isValidHeight(field.value) else { return false }
+            case "hcl": guard isValidHairColor(field.value) else { return false }
+            case "ecl": guard isValidEyeColor(field.value) else { return false }
+            case "pid": guard isValidPassportId(field.value) else { return false }
+            default: break
+        }
+    }
+    return true
+}
+
+// main
+func buildDocument(_ str: String) -> [String: String] {
+    str.components(separatedBy: .whitespacesAndNewlines).reduce(into: [:]) { result, field in
         let parts = field.components(separatedBy: ":")
         result[parts[0]] = parts[1]
     }
-
-    func isYear(at field: String, within range: ClosedRange<Int>) -> Bool {
-        guard let year = Int(fields[field, default: ""]) else { return false }
-        return range.contains(year)
-    }
-
-    func isHeightValid() -> Bool {
-        guard let height = fields["hgt"] else { return false }
-        guard let value = Int(height.dropLast(2)) else { return false }
-
-        switch height.suffix(2) {
-            case "in": return (59...76).contains(value)
-            case "cm": return (150...193).contains(value)
-            default: return false
-        }
-    }
-
-    func isHairColorValid() -> Bool {
-        guard let color = fields["hcl"] else { return false }
-        return color.first == "#" && Int(color.dropFirst(), radix: 16) != nil
-    }
-
-    func isEyeColorValid() -> Bool {
-        guard let color = fields["ecl"] else { return false }
-        return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(color)
-    }
-
-    func isPassportIdValid() -> Bool {
-        guard let id = fields["pid"] else { return false }
-        return id.count == 9 && Int(id) != nil
-    }
-
-    return isYear(at: "byr", within: 1920...2002) &&
-        isYear(at: "iyr", within: 2010...2020) &&
-        isYear(at: "eyr", within: 2020...2030) &&
-        isHeightValid() &&
-        isHairColorValid() &&
-        isEyeColorValid() &&
-        isPassportIdValid()
 }
 
-let docs = input.components(separatedBy: "\n\n")
+let docs = input.components(separatedBy: "\n\n").map(buildDocument)
 
-let partOneValidCount = docs.count(where: isValidPartOneDoc)
+let partOneValidCount = docs.count(where: hasRequiredFields)
 print("Answer to part one: \(partOneValidCount)")
 
-let partTwoValidCount = docs.count(where: isValidPartTwoDoc)
+let partTwoValidCount = docs.count(where: hasValidFields)
 print("Answer to part two: \(partTwoValidCount)")
---
+++ day4/Sources/day4/orig.swift
@@ -0,0 +1,48 @@
+//
+//  old.swift
+//  
+//
+//  Created by Jake Foster on 12/4/20.
+//
+
+func isValidPartTwoFieldsOrig(_ fields: [String: String]) -> Bool {
+    func isYear(at field: String, within range: ClosedRange<Int>) -> Bool {
+        guard let year = Int(fields[field, default: ""]) else { return false }
+        return range.contains(year)
+    }
+
+    func isHeightValid() -> Bool {
+        guard let height = fields["hgt"] else { return false }
+        guard let value = Int(height.dropLast(2)) else { return false }
+
+        switch height.suffix(2) {
+            case "in": return (59...76).contains(value)
+            case "cm": return (150...193).contains(value)
+            default: return false
+        }
+    }
+
+    func isHairColorValid() -> Bool {
+        guard let color = fields["hcl"] else { return false }
+        return color.first == "#" && Int(color.dropFirst(), radix: 16) != nil
+    }
+
+    func isEyeColorValid() -> Bool {
+        guard let color = fields["ecl"] else { return false }
+        return validEyeColors.contains(color)
+    }
+
+    func isPassportIdValid() -> Bool {
+        guard let id = fields["pid"] else { return false }
+        return id.count == 9 && Int(id) != nil
+    }
+
+    return isYear(at: "byr", within: 1920...2002) &&
+        isYear(at: "iyr", within: 2010...2020) &&
+        isYear(at: "eyr", within: 2020...2030) &&
+        isHeightValid() &&
+        isHairColorValid() &&
+        isEyeColorValid() &&
+        isPassportIdValid()
+}
+
