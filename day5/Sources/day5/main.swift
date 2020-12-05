df2de9c7ba0e04b28319d5b803d0874407457a6f
2BJake <jakef@fastmail.com>
2020-12-05 08:05:07 -0600

    renaming

--- day5/Sources/day5/main.swift
+++ day5/Sources/day5/main.swift
@@ -1,33 +1,33 @@
 import Foundation
 
 extension Collection {
-    func dropBackHalf() -> SubSequence {
-        dropLast(count/2)
+    func dropFirstHalf() -> SubSequence {
+        dropFirst(count/2)
     }
 
-    func dropFrontHalf() -> SubSequence {
-        dropFirst(count/2)
+    func dropLastHalf() -> SubSequence {
+        dropLast(count/2)
     }
 }
 
-func seatId(from boarding: [Character]) -> Int {
-    let row = boarding.prefix(8).reduce((0...127)[...]) { range, value in
-        value == "F" ? range.dropBackHalf() : range.dropFrontHalf()
+func calculateSeatId(from boardingPass: [Character]) -> Int {
+    let row = boardingPass.prefix(8).reduce((0...127)[...]) { range, value in
+        value == "F" ? range.dropLastHalf() : range.dropFirstHalf()
     }.first!
 
-    let col = boarding.suffix(3).reduce((0...7)[...]) { range, value in
-        value == "L" ? range.dropBackHalf() : range.dropFrontHalf()
+    let col = boardingPass.suffix(3).reduce((0...7)[...]) { range, value in
+        value == "L" ? range.dropLastHalf() : range.dropFirstHalf()
     }.first!
 
     return row * 8 + col
 }
 
 let boardingPasses = input.split(separator: "\n").map(Array.init)
-let seatIds = boardingPasses.map(seatId)
+let seatIds = boardingPasses.map(calculateSeatId)
 
 print("answer to part one: \(seatIds.max())")
 
-func missingSeat(in seatIds: [Int]) -> Int? {
+func findMissingSeat(in seatIds: [Int]) -> Int? {
     let seatIds = seatIds.sorted()
     for i in 0..<(seatIds.count - 1) {
         let nextSeatId = seatIds[i] + 1
@@ -38,4 +38,4 @@ func missingSeat(in seatIds: [Int]) -> Int? {
     return nil
 }
 
-print("answer to part two: \(missingSeat(in: seatIds))")
+print("answer to part two: \(findMissingSeat(in: seatIds))")
