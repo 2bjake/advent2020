0002e62f70a25e745ef7b84eeb69152b84f5ff03
2BJake <jakef@fastmail.com>
2020-12-05 10:48:03 -0600

    alt solutions for day 5

--- day5/Sources/day5/main.swift
+++ day5/Sources/day5/main.swift
@@ -39,3 +39,52 @@ func findMissingSeat(in seatIds: [Int]) -> Int? {
 }
 
 print("answer to part two: \(findMissingSeat(in: seatIds))")
+
+/// Alt: Define "find missing" as a generic method on Collection. There's nothing seat-specific about the algorithm.
+
+extension Collection where Element: Comparable & Strideable {
+    /// Returns the first value that is not present in the `Sequence`
+    /// while traversing `Element`s in order of value.
+    ///
+    /// Complexity: O(n log n), where n is the length of the sequence.
+    func findFirstMissing() -> Element? where Element.Stride: ExpressibleByIntegerLiteral {
+        let ordered = sorted()
+        for i in 0..<(ordered.count - 1) {
+            let nextValue = ordered[i].advanced(by: 1)
+            if ordered[i + 1] != nextValue {
+                return nextValue
+            }
+        }
+        return nil
+    }
+}
+
+print("answer to part two: \(seatIds.findFirstMissing())")
+
+/// Alt: Use subtraction from range to find missing value - O(n) solution
+
+func findMissingSeatWitRange(in seatIds: [Int]) -> Int? {
+    guard let lowerBound = seatIds.min(),
+          let upperBound = seatIds.max() else { return nil }
+
+    return Set(lowerBound...upperBound).subtracting(seatIds).min()
+}
+
+print("answer to part two: \(findMissingSeatWitRange(in: seatIds))")
+
+/// Alt: O(n) solution on Collection
+
+extension Collection where Element: Hashable & Strideable {
+    /// Returns the first value that is not present in the `Sequence`
+    /// while traversing `Element`s in order of value.
+    ///
+    /// Complexity: O(n), where n is the length of the sequence.
+    func findFirstMissingLinear() -> Element? where Element.Stride: SignedInteger {
+        guard let lowerBound = self.min(),
+              let upperBound = self.max() else { return nil }
+
+        return Set(lowerBound...upperBound).subtracting(self).min()
+    }
+}
+
+print("answer to part two: \(seatIds.findFirstMissingLinear())")
