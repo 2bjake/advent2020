3f53926a05ab2fa76c5ace5ff8b71e8c6077b27b
2BJake <jakef@fastmail.com>
2020-12-05 07:42:57 -0600

    minor cleanup

--- day5/Sources/day5/main.swift
+++ day5/Sources/day5/main.swift
@@ -11,32 +11,31 @@ extension Collection {
 }
 
 func seatId(from boarding: [Character]) -> Int {
-    let row = boarding.prefix(8).reduce((0...127)[...]) { result, value in
-        value == "F" ? result.dropBackHalf() : result.dropFrontHalf()
+    let row = boarding.prefix(8).reduce((0...127)[...]) { range, value in
+        value == "F" ? range.dropBackHalf() : range.dropFrontHalf()
     }.first!
 
-    let col = boarding.suffix(3).reduce((0...7)[...]) { result, value in
-        value == "L" ? result.dropBackHalf() : result.dropFrontHalf()
+    let col = boarding.suffix(3).reduce((0...7)[...]) { range, value in
+        value == "L" ? range.dropBackHalf() : range.dropFrontHalf()
     }.first!
 
     return row * 8 + col
 }
 
-
-
 let boardingPasses = input.split(separator: "\n").map(Array.init)
-
 let seatIds = boardingPasses.map(seatId)
 
 print("answer to part one: \(seatIds.max())")
 
 func missingSeat(in seatIds: [Int]) -> Int? {
+    let seatIds = seatIds.sorted()
     for i in 0..<(seatIds.count - 1) {
-        if seatIds[i + 1] != seatIds[i] + 1 {
-            return seatIds[i] + 1
+        let nextSeatId = seatIds[i] + 1
+        if seatIds[i + 1] != nextSeatId {
+            return nextSeatId
         }
     }
     return nil
 }
 
-print("answer to part two: \(missingSeat(in: seatIds.sorted()))")
+print("answer to part two: \(missingSeat(in: seatIds))")
