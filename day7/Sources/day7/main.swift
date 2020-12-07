358f5b324117cadafcce15d418ef1857af6ab777
2BJake <jakef@fastmail.com>
2020-12-07 10:33:41 -0600

    day 7 minor cleanup

--- day7/Sources/day7/main.swift
+++ day7/Sources/day7/main.swift
@@ -2,29 +2,27 @@ import Foundation
 
 let registry = BagRegistry(bags: input.split(separator: "\n").compactMap(Bag.build))
 
-func partOne() {
-    var ancestors: Set<String> = []
+// part one
+var ancestors: Set<String> = []
 
-    func collectAncestors(of bag: Bag) {
-        for parent in registry.retrieveParents(of: bag) {
-            ancestors.insert(parent.color)
-            collectAncestors(of: parent)
-        }
+func collectAncestors(of bag: Bag) {
+    for parent in registry.retrieveParents(of: bag) {
+        ancestors.insert(parent.color)
+        collectAncestors(of: parent)
     }
-
-    collectAncestors(of: registry.shinyGoldBag)
-    print("answer to part one: \(ancestors.count)") // 372
 }
-partOne()
 
-func partTwo() {
-    func calculateCount(for bag: Bag) -> Int {
-        bag.contents.reduce(0) { result, content in
-            guard let child = registry.retrieveBag(withColor: content.color) else { return result }
-            return result + content.count * (calculateCount(for: child) + 1)
-        }
+collectAncestors(of: registry.shinyGoldBag)
+print("answer to part one: \(ancestors.count)") // 372
+
+
+// part two
+func calculateCount(for bag: Bag) -> Int {
+    bag.contents.reduce(0) { result, content in
+        guard let child = registry.retrieveBag(withColor: content.color) else { return result }
+        return result + content.count * (calculateCount(for: child) + 1)
     }
-
-    print("answer to part two: \(calculateCount(for: registry.shinyGoldBag))") // 8015
 }
-partTwo()
+
+print("answer to part two: \(calculateCount(for: registry.shinyGoldBag))") // 8015
+
