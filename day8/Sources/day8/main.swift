46d1b8d37978804d29b67404debe01c734728504
2BJake <jakef@fastmail.com>
2020-12-08 05:47:39 -0600

    day 8 part 2 working

--- day8/Sources/day8/main.swift
+++ day8/Sources/day8/main.swift
@@ -16,14 +16,24 @@ extension Instruction {
         }
         self.init(op: op, arg: arg)
     }
+
+    func flipped() -> Instruction {
+        switch self.op {
+            case .nop: return Instruction(op: .jmp, arg: arg)
+            case .jmp: return Instruction(op: .nop, arg: arg)
+            case .acc: return self
+        }
+    }
 }
 
-func findAccBeforeLoop(_ instructions: [Instruction]) -> Int {
+typealias ExecutionResult = (accumulator: Int, successful: Bool, executedIndicies: Set<Int>)
+
+func execute(_ instructions: [Instruction]) -> ExecutionResult {
     var accumulator = 0
     var index = 0
     var executedIndicies: Set<Int> = []
 
-    while !executedIndicies.contains(index) {
+    while !executedIndicies.contains(index) && index < instructions.count {
         executedIndicies.insert(index)
         let arg = instructions[index].arg
         switch instructions[index].op {
@@ -31,10 +41,34 @@ func findAccBeforeLoop(_ instructions: [Instruction]) -> Int {
             case .acc: accumulator += arg; fallthrough
             case .nop: index += 1
         }
+
     }
-    return accumulator
+    return (accumulator, index == instructions.count, executedIndicies)
 }
 
 let instructions = input.split(separator: "\n").compactMap(Instruction.init)
 
-print(findAccBeforeLoop(instructions))
+func partOne() {
+    let result = execute(instructions)
+    print(result.accumulator)
+}
+
+// part 2
+
+func partTwo() -> Int? {
+    let result = execute(instructions)
+    for badInstructionIdx in result.executedIndicies {
+        let badInstruction = instructions[badInstructionIdx]
+        if badInstruction.op == .jmp || badInstruction.op == .nop {
+            var newInstructions = instructions
+            newInstructions[badInstructionIdx] = badInstruction.flipped()
+            let newResult = execute(newInstructions)
+            if newResult.successful {
+                return newResult.accumulator
+            }
+        }
+    }
+    return nil
+}
+
+print(partTwo())
