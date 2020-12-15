5f87c6fb61191f888a791237619121bcef5eb1bd
2BJake <jakef@fastmail.com>
2020-12-15 09:55:56 -0600

    day15 second part done

--- day15/Sources/day15/main.swift
+++ day15/Sources/day15/main.swift
@@ -1,21 +1,27 @@
-let values = [1, 17, 0, 10, 18, 11, 6]
 
-var counter = 0
+func findNthSpoken(_ n: Int, startingValues: [Int]) -> Int {
+    var counter = 0
 
-var lastTurnByNumber = [Int: Int]()
+    var lastTurnByNumber = [Int: Int]()
 
-for value in values {
-    counter += 1
-    lastTurnByNumber[value] = counter
+    for value in startingValues {
+        counter += 1
+        lastTurnByNumber[value] = counter
+    }
+
+    var lastSpoken = startingValues.last!
+
+    for _ in counter..<n {
+        let lastTurnSpoken = lastTurnByNumber[lastSpoken, default: counter]
+        lastTurnByNumber[lastSpoken] = counter
+        lastSpoken = counter - lastTurnSpoken
+        counter += 1
+    }
+    return lastSpoken
 }
 
-var lastSpoken = values.last!
+let input = [1, 17, 0, 10, 18, 11, 6]
 
-for _ in counter..<2020 {
-    let lastTurnSpoken = lastTurnByNumber[lastSpoken, default: counter]
-    lastTurnByNumber[lastSpoken] = counter
-    lastSpoken = counter - lastTurnSpoken
-    counter += 1
-}
+print("answer to part one: \(findNthSpoken(2020, startingValues: input))") // 595
+print("answer to part two: \(findNthSpoken(30_000_000, startingValues: input))") // 1708310
 
-print(lastSpoken)
