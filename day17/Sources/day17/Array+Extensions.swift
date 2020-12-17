27513d7245becb38f2bbcd7cb377927c0de08d00
2BJake <jakef@fastmail.com>
2020-12-17 06:39:59 -0600

    day 17 part 2 working

--- day17/Sources/day17/Array+Extensions.swift
+++ day17/Sources/day17/Array+Extensions.swift
@@ -64,10 +64,61 @@ extension Sequence {
         self.filter(predicate).count
     }
 }
-//
-//extension Sequence where Element: Sequence {
-//    func count(where predicate: (Element.Element) -> Bool) -> Int {
-//        flatMap { $0 }.count(predicate)
-//    }
-//}
 
+// =============
+
+typealias HyperPoint = (x: Int, y: Int, z: Int, w: Int)
+typealias HyperSlope = (Δx: Int, Δy: Int, Δz: Int, Δw: Int)
+
+func +(point: HyperPoint, slope: HyperSlope) -> HyperPoint {
+    HyperPoint(x: point.x + slope.Δx, y: point.y + slope.Δy, z: point.z + slope.Δz, w: point.w + slope.Δw)
+}
+
+func +=(point: inout HyperPoint, slope: HyperSlope) {
+    point = point + slope
+}
+
+extension Array where Element: RandomAccessCollection & MutableCollection, Element.Index == Int, Element.Element: RandomAccessCollection & MutableCollection, Element.Element.Index == Int, Element.Element.Element: RandomAccessCollection & MutableCollection, Element.Element.Element.Index == Int {
+    typealias HyperBaseElement = Element.Element.Element.Element
+
+    subscript(_ point: HyperPoint) -> HyperBaseElement {
+        get { self[point.y][point.x][point.z][point.w] }
+        set { self[point.y][point.x][point.z][point.w] = newValue }
+    }
+
+    func element(at point: HyperPoint) -> HyperBaseElement? {
+        guard indices.contains(point.y) && self[0].indices.contains(point.x) && self[0][0].indices.contains(point.z) && self[0][0][0].indices.contains(point.w) else { return nil }
+        return self[point]
+    }
+
+    func adjacentElements(of point: HyperPoint) -> [HyperBaseElement] {
+        var neighbors = [HyperBaseElement]()
+        for x in -1...1 {
+            for y in -1...1 {
+                for z in -1...1 {
+                    for w in -1...1 {
+                        let newPoint = point + (x, y, z, w)
+                        if newPoint != point, let neighbor = element(at: newPoint) {
+                            neighbors.append(neighbor)
+                        }
+                    }
+                }
+            }
+        }
+        return neighbors
+    }
+
+    var allHyperPoints: [HyperPoint] {
+        var result = [HyperPoint]()
+        for x in 0..<self[0].count {
+            for y in 0..<self.count {
+                for z in 0..<self[0][0].count {
+                    for w in 0..<self[0][0][0].count {
+                        result.append(HyperPoint(x, y, z, w))
+                    }
+                }
+            }
+        }
+        return result
+    }
+}
--- day17/Sources/day17/main.swift
+++ day17/Sources/day17/main.swift
@@ -6,21 +6,42 @@ let initialValues = input.split(separator: "\n").map { row in
 
 let iterations = 6
 
-let height = initialValues.count + 2 * iterations
-let width = initialValues[0].count + 2 * iterations
-let depth = 1 + 2 * iterations
+func buildGrid() -> [[[Bool]]] {
+    let height = initialValues.count + 2 * iterations
+    let width = initialValues[0].count + 2 * iterations
+    let depth = 1 + 2 * iterations
 
-let zRow = Array(repeating: false, count: depth)
-let xRow = Array(repeating: zRow, count: width)
-var grid = Array(repeating: xRow, count: height)
+    let zRow = Array(repeating: false, count: depth)
+    let xRow = Array(repeating: zRow, count: width)
+    var grid = Array(repeating: xRow, count: height)
 
-for y in 0..<initialValues.count {
-    for x in 0..<initialValues[0].count {
-        grid[Point(x + iterations, y + iterations, iterations)] = initialValues[y][x]
+    for y in 0..<initialValues.count {
+        for x in 0..<initialValues[0].count {
+            grid[Point(x + iterations, y + iterations, iterations)] = initialValues[y][x]
+        }
     }
+    return grid
 }
 
-func determineNewGrid(_ cubes: [[[Bool]]]) -> [[[Bool]]] {
+func buildHyperGrid() -> [[[[Bool]]]] {
+    let height = initialValues.count + 2 * iterations
+    let width = initialValues[0].count + 2 * iterations
+    let depth = 1 + 2 * iterations
+
+    let wRow = Array(repeating: false, count: depth)
+    let zRow = Array(repeating: wRow, count: depth)
+    let xRow = Array(repeating: zRow, count: width)
+    var grid = Array(repeating: xRow, count: height)
+
+    for y in 0..<initialValues.count {
+        for x in 0..<initialValues[0].count {
+            grid[HyperPoint(x + iterations, y + iterations, iterations, iterations)] = initialValues[y][x]
+        }
+    }
+    return grid
+}
+
+func determineNewGrid(_ grid: [[[Bool]]]) -> [[[Bool]]] {
     var newGrid = grid
     for point in grid.allPoints {
         let neighbors = grid.adjacentElements(of: point)
@@ -36,12 +57,47 @@ func determineNewGrid(_ cubes: [[[Bool]]]) -> [[[Bool]]] {
     return newGrid
 }
 
-for _ in 0..<6 {
-    grid = determineNewGrid(grid)
+func partOne() {
+    var grid = buildGrid()
+    for _ in 0..<6 {
+        grid = determineNewGrid(grid)
+    }
+
+    let count = grid.allPoints.reduce(0) { result, value in
+        grid[value] ? result + 1 : result
+    }
+
+    print(count) // 315
+}
+partOne()
+
+
+func determineNewHyperGrid(_ grid: [[[[Bool]]]]) -> [[[[Bool]]]] {
+    var newGrid = grid
+    for point in grid.allHyperPoints {
+        let neighbors = grid.adjacentElements(of: point)
+        let activeCount = neighbors.count { $0 }
+
+        if grid[point] && (activeCount != 2 && activeCount != 3) {
+            newGrid[point] = false
+        } else if !grid[point] && activeCount == 3 {
+            newGrid[point] = true
+        }
+
+    }
+    return newGrid
 }
 
-let count = grid.allPoints.reduce(0) { result, value in
-    grid[value] ? result + 1 : result
-}
+func partTwo() {
+    var grid = buildHyperGrid()
+    for _ in 0..<6 {
+        grid = determineNewHyperGrid(grid)
+    }
 
-print(count)
+    let count = grid.allHyperPoints.reduce(0) { result, value in
+        grid[value] ? result + 1 : result
+    }
+
+    print(count) // 1520
+}
+partTwo()
